421344ed9e15eae9c1a9f59bddd1ff85c284598b
Poilaupat <ruben.delapille@gmail.com>
2024-07-02 23:43:58 +0200

    Finding duplicates

--- DevToysDuplicateFinder/DuplicateFinderExtension/DuplicateFinderGui.cs
+++ DevToysDuplicateFinder/DuplicateFinderExtension/DuplicateFinderGui.cs
@@ -24,11 +24,24 @@ namespace DuplicateFinderExtension
     )]
     internal sealed class DuplicateFinderGui : IGuiTool
     {
-        public UIToolView View => new(Label().Style(UILabelStyle.BodyStrong).Text(DuplicateFinderExtension.HelloWorldLabel));
+        public UIToolView View
+            //=> new(Label().Style(UILabelStyle.BodyStrong).Text(DuplicateFinderExtension.HelloWorldLabel));
+            => new(
+                Stack()
+                .Vertical()
+                .WithChildren(
+                    MultiLineTextInput().Title("Input").Extendable().OnTextChanged(OnInputTextChanged),
+                    MultiLineTextInput().Title("Found duplicates").ReadOnly().HideCommandBar().NeverShowLineNumber())
+                );
 
         public void OnDataReceived(string dataTypeName, object? parsedData)
         {
             throw new NotImplementedException();
+        }
+
+        private void OnInputTextChanged(string text)
+        {
+
         }
     }
 }
