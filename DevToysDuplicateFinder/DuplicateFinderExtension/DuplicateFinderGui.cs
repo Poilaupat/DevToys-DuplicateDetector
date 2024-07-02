fa2e0810548d8ba2fa20520fcfd84c054dfcdbcf
Ruben Delapille <ruben.delapille@tessi.fr>
2024-07-02 15:33:53 +0200

    Minimal extension

---
+++ DevToysDuplicateFinder/DuplicateFinderExtension/DuplicateFinderGui.cs
@@ -0,0 +1,34 @@
+﻿using DevToys.Api;
+using static DevToys.Api.GUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateFinderExtension
+{
+    [Export(typeof(IGuiTool))]
+    [Name("DuplicateFinder")]
+    [ToolDisplayInformation(
+        IconFontName = "FluentSystemIcon", 
+        IconGlyph = '\uE670', 
+        GroupName = PredefinedCommonToolGroupNames.Text,
+        ResourceManagerAssemblyIdentifier = nameof(ResourceAssemblyIdentifier),
+        ResourceManagerBaseName = "DuplicateFinderExtension.DuplicateFinderExtension",
+        ShortDisplayTitleResourceName = nameof(DuplicateFinderExtension.ShortDisplayTitle),
+        LongDisplayTitleResourceName = nameof(DuplicateFinderExtension.LongDisplayTitle),
+        DescriptionResourceName = nameof(DuplicateFinderExtension.Description),
+        AccessibleNameResourceName = nameof(DuplicateFinderExtension.AccessibleName)
+    )]
+    internal sealed class DuplicateFinderGui : IGuiTool
+    {
+        public UIToolView View => new(Label().Style(UILabelStyle.BodyStrong).Text(DuplicateFinderExtension.HelloWorldLabel));
+
+        public void OnDataReceived(string dataTypeName, object? parsedData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevToysDuplicateFinder/DuplicateFinderExtension/ResourceAssemblyIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using DevToys.Api;
+using System.ComponentModel.Composition;
+
+namespace DuplicateFinderExtension
+{
+    [Export(typeof(IResourceAssemblyIdentifier))]
+    [Name(nameof(ResourceAssemblyIdentifier))]
+    internal sealed class ResourceAssemblyIdentifier : IResourceAssemblyIdentifier
+    {
+        public ValueTask<FontDefinition[]> GetFontDefinitionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
